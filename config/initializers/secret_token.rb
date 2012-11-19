a1f4cabf62f8b20f681d4d3e8837d59ec518b47c
Guillermo Morales <gmoralestinoco@gmail.com>
2012-11-19 18:13:50 -0500

    HAsta las 6 de la tarde, en el transcurso de la noche enviare mejoras

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Elblog::Application.config.secret_token = 'd479737a0c978aa81e99355efba28a6462e3162c61013cb04fe188750a2803a81f32b77878784628ba0570908a785029c25772be3f3d9b9b2c3114179648f0e7'
